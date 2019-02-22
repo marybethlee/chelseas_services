477a956d256355fabc5ea37d60663aa6d6ab458a
Mary Lee <marybethlee11@gmail.com>
2019-02-22 14:35:56 -0500

    Add rels to target blank links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,16 @@ const Footer = (props) => (
   <footer id="footer">
     <div className="inner">
       <ul className="icons">
-        <li><a href="https://www.facebook.com/chelseastaxservices/" target="_blank" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-        <li><a href="https://www.linkedin.com/in/chelsea-rustek-137356b5/" target="_blank" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+        <li>
+          <a href="https://www.facebook.com/chelseastaxservices/" target="_blank" className="icon alt fa-facebook" rel="noopener noreferrer">
+            <span className="label">Facebook</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/chelsea-rustek-137356b5/" target="_blank" className="icon alt fa-linkedin" rel="noopener noreferrer">
+            <span className="label">LinkedIn</span>
+          </a>
+        </li>
       </ul>
       <ul className="copyright">
         <li>&copy; {new Date().getFullYear()} Chelsea's Tax Services</li>
